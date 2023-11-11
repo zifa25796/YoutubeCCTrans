38fef6f6e740b96380d6be17bf0e52752ec83e7f
Ziheng Fan <zfan88@uwo.ca>
2023-11-11 00:21:01 -0500

    Testing Code

--- Flask/src/ServerStartUp.py
+++ Flask/src/ServerStartUp.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Root"
+    return jsonify({'status': 500})
 
 if __name__ == '__main__':
     app.run(debug=True)
