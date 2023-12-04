4e7882d2a403458c38652bc56e2c381b17f2cc6a
Ziheng Fan <zfan88@uwo.ca>
2023-12-04 14:50:38 -0500

    Change Extension UI

--- chrome-extantion-react/src/App.tsx
+++ chrome-extantion-react/src/App.tsx
@@ -1,26 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [vid, setVid] = useState("")
+
+  function getVedioID(str = "") {
+    const params = str.split("?")[1]!
+    const paramPairs = params.split("&")
+
+    if (paramPairs) {
+      paramPairs.forEach(param => {
+        let temp = param.split("=")
+        if (temp[0] === "v") {
+          console.log(temp[1])
+          setVid(temp[1])
+        }
+      })
+    }
+  }
+
+  const tryPost = () => {
+    getVedioID("https://www.youtube.com/watch?v=Oz4XfhktPBI")
+    fetch('http://127.0.0.1:5000/test', {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: vid
+      })
+    }).then(res => res.json()).then(data => (
+      console.log(data)
+    ))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='App'>
+      <div>
+        <head>
+          <link rel="stylesheet" href="style.css" />
+        </head>
+        <body>
+
+          <div id="header">
+            <h1>YoutubeCCTrans</h1>
+            <div></div>
+          </div>
+
+          <div id="navBar">
+            <div>
+              <div>
+                <div>CC Grab</div>
+                <div>Summary</div>
+                <div>Other</div>
+              </div>
+            </div>
+          </div>
+
+          <div id="progressBar">
+            <div>
+              <h2>Grab CC</h2>
+              <div className="timeinterval">
+                <div>
+                  <p>Strat Time</p>
+                  <input type="text" />
+                </div>
+                <div>
+                  <p>End Time</p>
+                  <input type="text" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="translatePanel">
+            <div className="lan-transfer">
+              <select>
+                <option>Current Language</option>
+              </select>
+              <select>
+                <option>Translate Language</option>
+              </select>
+            </div>
+            <div className="trans-detail">
+              <div></div>
+              <div></div>
+            </div>
+
+          </div>
+          <div id="export">
+            <input type="text" value="file name" />
+            <button>Export</button>
+          </div>
+
+
+          <button id="test" onClick={tryPost}>click me</button>
+          <script type="text/javascript" src="popup.js"></script>
+        </body>
+      </div>
+    </div >
+  )
 }
 
 export default App;
